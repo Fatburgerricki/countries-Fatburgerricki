9b594112646b4e087ea05e21bfcd9f7d62752027
Ricki Sosa <rickis37727@students.laalliance.org>
2017-11-17 17:21:42 +0000

    finish

---
+++ countries.js
@@ -0,0 +1,9 @@
+var fourLetterCountries = [ 
+    "Chad", "Cuba", "Greenland", "Iraq", 
+    "Mali", "Oman", "India"
+    ]
+    
+    
+    countries.push("Fiji")
+    countries.length 
+    console.org(countries)
